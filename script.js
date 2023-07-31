3da9485caefb94810951d3c84f9d09413d4b6b38
David Forbes <davidforbesi@hotmail.com>
2023-07-31 16:36:13 +0100

    tidy some code away into separate fuctions

--- script.js
+++ script.js
@@ -18,60 +18,56 @@ function switchActivePlayer() {
   h1.textContent = `${player}' turn...`;
 };
 
-const cells = document.querySelectorAll('.game-cell');
+(function () {
+  const cells = document.querySelectorAll('.game-cell');
+  cells.forEach(button => button.addEventListener('click', () => {
+    const cellIndex = button.getAttribute('id').slice(5);
+    if (gameBoard[cellIndex - 1] !== '') return;
+    gameBoard[cellIndex - 1] = activePlayer;
+    switchActivePlayer();
+    displayBoard();
+    checkWinner();
+  }))
+})();
 
-cells.forEach(button => button.addEventListener('click', () => {
-  const cellIndex = button.getAttribute('id').slice(5);
-  gameBoard[cellIndex - 1] = activePlayer;
-  switchActivePlayer();
-  displayBoard();
-  checkWinner();
-}))
 
 function checkWinner() {
   if (gameBoard[0] == gameBoard[1] && gameBoard[1] == gameBoard[2] && gameBoard[0] !== "") {
-    displayWinner();
-    document.querySelector('#top-row').style.display = 'block';
+    displayWinner('#top-row');
   } else if (gameBoard[3] == gameBoard[4] && gameBoard[4] == gameBoard[5] && gameBoard[3] !== "") {
-    displayWinner();
-    document.querySelector('#middle-row').style.display = 'block';
+    displayWinner('#middle-row');
   } else if (gameBoard[6] == gameBoard[7] && gameBoard[7] == gameBoard[8] && gameBoard[6] !== "") {
-    displayWinner();
-    document.querySelector('#bottom-row').style.display = 'block';
+    displayWinner('#bottom-row');
   } else if (gameBoard[0] == gameBoard[3] && gameBoard[3] == gameBoard[6] && gameBoard[0] !== "") {
-    displayWinner();
-    document.querySelector('#left-column').style.display = 'block';
+    displayWinner('#left-column');
   } else if (gameBoard[1] == gameBoard[4] && gameBoard[4] == gameBoard[7] && gameBoard[1] !== "") {
-    displayWinner();
-    document.querySelector('#middle-column').style.display = 'block';
+    displayWinner('#middle-column');
   } else if (gameBoard[2] == gameBoard[5] && gameBoard[5] == gameBoard[8] && gameBoard[2] !== "") {
-    displayWinner();
-    document.querySelector('#right-column').style.display = 'block';
+    displayWinner('#right-column');
   } else if (gameBoard[0] == gameBoard[4] && gameBoard[4] == gameBoard[8] && gameBoard[0] !== "") {
-    displayWinner();
-    document.querySelector('#diagonal-1').style.display = 'block';
+    displayWinner('#diagonal-1');
   } else if (gameBoard[2] == gameBoard[4] && gameBoard[4] == gameBoard[6] && gameBoard[2] !== "") {
-    displayWinner();
-    document.querySelector('#diagonal-2').style.display = 'block';
+    displayWinner('#diagonal-2');
   } else if (gameBoard.every(cell => cell !== '')) {
     const h1 = document.querySelector('h1');
     h1.textContent = `It's a tie!`;
-    const cells = document.querySelectorAll('button.game-cell');
-    cells.forEach(cell => cell.disabled = true);
+    disableButtons();
   }
 }
 
-function displayWinner() {
+function displayWinner(id) {
+  document.querySelector(id).style.display = 'block';
   switchActivePlayer();
   const h1 = document.querySelector('h1');
   winner = activePlayer == 'X' ? 'Crosses' : 'Noughts'
-  h1.textContent = `${winner} won!`;
+  h1.textContent = `${winner} win!`;
+  disableButtons();
+}
+
+function disableButtons() {
   const cells = document.querySelectorAll('button.game-cell');
   cells.forEach(cell => cell.disabled = true);
 }
 
 const startAgain = document.querySelector('#start-again-button');
-
-startAgain.addEventListener('click', () => {
-  location.reload();
-})
+startAgain.addEventListener('click', () => location.reload());
